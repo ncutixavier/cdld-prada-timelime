4c8efff9dcae6164435aa0d7abdf4259aad3c1d4
ncuti60@gmail.com <ncuti60@gmail.com>
2023-03-06 17:59:21 +0200

    remove all logs

--- src/plugins/breakpoint.js
+++ src/plugins/breakpoint.js
@@ -1,6 +1,5 @@
 export const screens = () => {
   const screenWidth = window.innerWidth
-  console.log(screenWidth)
   return {
     mobile: screenWidth <= 425,
     tablet: screenWidth > 425 && screenWidth <= 768,
